87d2410205e0a2003eda99c289b407b2d8fec001
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-10-26 19:08:51 +0300

    intersection

---
+++ src/intersection.ts
@@ -0,0 +1,12 @@
+type Draggable = {
+    drag: () => void
+}
+type Resizable = {
+    resize: () => void
+}
+type UIWidget = Draggable & Resizable
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
