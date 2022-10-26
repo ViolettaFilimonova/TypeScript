cc5a1e22debf141ac323e649e0656587f76f14ab
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-10-26 18:57:17 +0300

    union

---
+++ src/union.js
@@ -0,0 +1,7 @@
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(10));
+console.log(kgToLbs('22kg'));
